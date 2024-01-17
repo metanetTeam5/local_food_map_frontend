0e79ee9f3419472e1b8bec3520f02b73e7dd822b
MinsunCho-dev <cms081222@gmail.com>
2024-01-17 16:08:38 +0900

    Feat: Home 프론트 완성

--- local_food_map_frontend/src/components/common/Footer.vue
+++ local_food_map_frontend/src/components/common/Footer.vue
@@ -1,33 +1,28 @@
 <template>
   <footer class="footer">
-
+    <div>
+      <div class="nav-links">
+        <a class="nav-link" href="/"
+          >아맛무제휴문의 | 이용약관 | 개인정보처리방침</a
+        >
+      </div>
+      <hr />
       <div>
-        <div class="nav-links">
-          <a class="nav-link" href="/">아맛무제휴문의 </a>
-        <span>|</span>
- 
-          <a class="nav-link" href="/">이용약관</a>
-          <span>|</span>
-          <a class="nav-link" href="/">개인정보처리방침</a>
-        </div>
-        <hr>
-          <div>
-          <span>(주) 아맛무팀</span> <span>&nbsp;|</span>
-          <span>&nbsp;소재지 : 서울특별시 종로구 창경궁로 254 </span>
-        </div>
-        <div>이메일문의 : metameta@metanet.com</div>
-        <div>
-          <p>전화문의 : 02-123-4567 (평일 : 10:00~19:00, 주말/공휴일 제외)</p>
-          <p>Copyright Ⓒ 2024 METANET TEAM AMATMU</p>
-  
-        </div>
-      </div>       
+        <span>(주) 아맛무팀</span> <span>&nbsp;|</span>
+        <span>&nbsp;소재지 : 서울특별시 종로구 창경궁로 254 </span>
+      </div>
+      <div>이메일문의 : metameta@metanet.com</div>
+      <div>
+        <p>전화문의 : 02-123-4567 (평일 : 10:00~19:00, 주말/공휴일 제외)</p>
+        <p>Copyright Ⓒ 2024 METANET TEAM AMATMU</p>
+      </div>
+    </div>
   </footer>
 </template>
 
-<script> 
+<script>
 export default {
-  name: 'AppFooter',
+  name: "AppFooter",
 };
 </script>
 
@@ -35,10 +30,10 @@ export default {
 .footer {
   width: 100%;
   padding: 20px;
-  background-color: #FEFEF8;
+  background-color: #fefef8;
   color: black;
   text-align: left;
-  font-family: 'BMHANNAPro';
+  font-family: "BMHANNAPro";
 }
 
 .nav-links {
